5e6bf2fcdb6bb29d177ef52a66ea47c46d4ed27c
Burit-Chaisiri <koolkoop2543@gmail.com>
2020-04-12 00:49:57 +0700

    Comment view_signup

--- Homepage/views.py
+++ Homepage/views.py
@@ -9,26 +9,29 @@ from django.forms import modelformset_factory
 from django.http import Http404
 from django.db.models import Count
 from django.contrib import messages
+
 class NoteWithThumbnail:
     def __init__(self, note, thumbnail):
         self.note = note
         self.thumbnail = thumbnail
 # Create your views here.
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        print(form.is_valid())
-        if form.is_valid():
-            newUser = form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            Profile.objects.create(user = newUser)
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('login')
+
+def signup(request): # สมัครไอดีที่จะเข้าใช้งาน
+    if request.method == 'POST': # ถ้า method ที่ได้มามีค่าเป็น POST 
+        form = UserCreationForm(request.POST)  #สร้าง formในการสมัคร
+        
+        if form.is_valid(): #เช็คว่าในform ถูกต้องมั้ย
+            newUser = form.save() #นำค่าที่ไปเก็บไว้
+            username = form.cleaned_data.get('username') # นำข้อความที่อยู่ใน text ที่มีไอดีว่า username มาเก็บไว้
+            raw_password = form.cleaned_data.get('password1')# นำข้อความที่อยู่ใน text ที่มีไอดีว่า password1 มาเก็บไว้
+            Profile.objects.create(user = newUser) #สร้าง Profileขึ้นมา
+            user = authenticate(username=username, password=raw_password) #กำหนดว่า user จะมี username กับ pass
+            login(request, user)#ทำการlogin โดยใช้userที่สมุคร
+            return redirect('login') #ไปยัง html ที่แสดงหน้าการเข้าสู้ระบบ
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'signup.html', {'form': form}) # ทำการ render html เพื่อแสดงหน้าสมัครไอดีที่ใช้งาน
+
 def home(request):
     noteWithThumbnail = []
     latestNote = []
@@ -135,75 +138,3 @@ def profile(request, username):
             if profileObj in note.userSaved.all():
                 savedNote.append(NoteWithThumbnail(note, note.Lecture_img.all()[0]))
     return render(request,'profile.html',{'form': form, 'profile': profileObj, 'myNote': myNote, 'savedNote':savedNote, 'saves':saves})
-
-'''List_object_Lecture=[]
-List_object_image={}
-List_object_image_value_dirt=[]
-List_name_img=[]
-
-
-def upload(request):
-    V=0
-    found=""
-    if Profile.objects.filter(user=request.user):
-        Profile_filter=Profile.objects.filter(user=request.user)
-        if request.method == 'POST' and 'uploadbutton' in request.POST:
-            LectureForms_upload=LectureForms(request.POST)
-            Lecture_imgForms_upload = Lecture_imgForms(request.POST,request.FILES)
-            if LectureForms_upload.is_valid() and  Lecture_imgForms_upload.is_valid() and request.FILES :
-                title_name=LectureForms_upload.cleaned_data.get("title")
-                for i in List_object_Lecture:
-                    if str(i)==title_name:
-                        found=str(i)
-                        V+=1
-
-                if V>0 :
-                    Lec=LectureForms_upload.save(commit=False)
-                    Lec.author=Profile_filter[0]
-                    Saveimg=Lecture_imgForms_upload.save()
-                    List_object_image_value_dirt.append(Saveimg)
-                    #Saveimg.LectureKey=Lec
-                    #List_object_image.update({title_name:List_object_image_value_dirt.append(Saveimg)})
-
-                else:
-                    Lec=LectureForms_upload.save(commit=False)
-                    Lec.author=Profile_filter[0]
-                    List_object_Lecture.append(Lec)
-                    Saveimg=Lecture_imgForms_upload.save()
-                    #Saveimg.LectureKey=Lec
-                    List_object_image_value_dirt.append(Saveimg)
-                List_name_img.append(Lecture_imgForms_upload.cleaned_data.get("image").name)
-                List_object_image.update({title_name:List_object_image_value_dirt})
-                return render(request, 'upload.html',{'Image':Lecture_imgForms_upload , 'Lecture':LectureForms_upload, 'L':List_object_Lecture ,"I":List_name_img})
-        elif request.method == 'POST' and 'submitbutton' in request.POST:
-            
-            LectureForms_upload_1=LectureForms(request.POST)
-            
-            if LectureForms_upload_1.is_valid():
-                if List_object_Lecture:
-                    title_name=LectureForms_upload_1.cleaned_data.get('title')
-                    for i in List_object_Lecture:
-                        if str(i)==title_name:
-                            i.save()
-                            
-                            if List_object_image:
-                                for P in List_object_image:
-                                    if P == title_name:
-                                        L=List_object_image[P]
-                                        for M in L:
-                                            M.LectureKey=i
-                                            M.save()
-                                List_object_image.pop(title_name)
-                            List_object_Lecture.remove(i)
-                
-            Obj_Lec=Lecture.objects.filter(title=LectureForms_upload_1.cleaned_data.get('title'))
-            Img_obj=Lecture_img.objects.all().filter(LectureKey=Obj_Lec[0])
-            List_object_Lecture.clear()
-            List_object_image.clear()
-            List_object_image_value_dirt.clear() 
-            List_name_img.clear()
-            return render(request, 'lecture.html',{"Lecture_img":Img_obj, 'Lecture':LectureForms_upload_1 })
-        else:
-            Lecture_imgForms1=Lecture_imgForms()
-            LectureForms1=LectureForms()
-            return render(request, 'upload.html',{'Image':Lecture_imgForms1 , 'Lecture':LectureForms1})'''
