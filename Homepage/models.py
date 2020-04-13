7666e020a93e7deb632460d680521f0ca87e7d58
Burit-Chaisiri <koolkoop2543@gmail.com>
2020-04-13 15:07:29 +0700

    Add comment model

--- Homepage/models.py
+++ Homepage/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+# Model ของ โปรไฟล์ของผู้ใช้งาน 
 class Profile(models.Model):
     user = models.OneToOneField(
         User,
@@ -12,19 +13,20 @@ class Profile(models.Model):
         default=None,
         null=True
     )
-    profilePicture = models.ImageField(upload_to="images/", blank=True, default=None, null=True)
+    profilePicture = models.ImageField(upload_to="images/", blank=True, default=None, null=True)  # รูปภาพของ ผู้ใช้งาน 
     def __str__(self):
-        return self.user.username
+        return self.user.username # ชื่อ ผู้ใช้งาน
 
+# Model ของ Lecture
 class Lecture(models.Model):
-    title = models.CharField(max_length=200,null=True)
-    subject = models.CharField(max_length=200,null=True)
-    description = models.CharField(max_length=2000,null=True)
+    title = models.CharField(max_length=200,null=True) # กล่องข้อความที่ใส่ชื่อชื่อเรื่อง
+    subject = models.CharField(max_length=200,null=True) # กล่องข้อความที่ใส่ชื่อวิชา
+    description = models.CharField(max_length=2000,null=True)# กล่องข้อความที่ใส่รายละเอียด 
     author = models.ForeignKey(Profile, related_name='author',on_delete=models.CASCADE,blank=True,null=True)
     userSaved = models.ManyToManyField(Profile)
     def __str__(self):
         return self.title
-
+# Model ของ รูปที่อัพพร้อม Lecture
 class Lecture_img(models.Model):
     LectureKey = models.ForeignKey(Lecture, related_name='Lecture_img',on_delete=models.CASCADE,blank=True,null=True)
     image = models.ImageField(upload_to='lecture_image',blank=True)
